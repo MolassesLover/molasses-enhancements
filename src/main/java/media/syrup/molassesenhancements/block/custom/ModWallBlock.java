8cca35638ed26779ef96c157d3ed20d526deb903
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-11-01 19:36:41 +0100

    Started working on wall blocks

---
+++ src/main/java/media/syrup/molassesenhancements/block/custom/ModWallBlock.java
@@ -0,0 +1,10 @@
+package media.syrup.molassesenhancements.block.custom;
+
+import net.minecraft.block.WallBlock;
+
+public class ModWallBlock extends WallBlock {
+
+    public ModWallBlock(Settings settings) {
+        super(settings);
+    }
+}
