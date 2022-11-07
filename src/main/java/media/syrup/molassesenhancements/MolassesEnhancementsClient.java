b07c6e31f81a2533e154be367030e933ed33e34b
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-11-07 19:21:27 +0100

    Created the client class

---
+++ src/main/java/media/syrup/molassesenhancements/MolassesEnhancementsClient.java
@@ -0,0 +1,13 @@
+package media.syrup.molassesenhancements;
+
+import media.syrup.molassesenhancements.block.ModBlocks;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+public class MolassesEnhancementsClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ORIENTAL_WINDOW_PANEL, RenderLayer.getCutout());
+    }
+}
