a4ab45f41ec762b569c5ef5398595dca8443f329
SoggiestCereal <Katieybug2@outlook.com>
2022-11-07 19:30:29 -0500

    Removed Ice Block transparency bug from angle

--- src/main/java/media/syrup/molassesenhancements/block/ModBlocks.java
+++ src/main/java/media/syrup/molassesenhancements/block/ModBlocks.java
@@ -145,7 +145,7 @@ public static final Block ROSE_QUARTZ_PILLAR_FLOWERS = registerBlock("rose_quart
                                                                      new ModPillarBlock(FabricBlockSettings.of(Material.STONE).hardness(0.15f).resistance(0.6f)), ItemGroup.BUILDING_BLOCKS);
 
 public static final Block ICE_BRICKS = registerBlock("ice_bricks",
-                                                     new Block(FabricBlockSettings.of(Material.ICE).hardness(0.15f).resistance(0.6f)), ItemGroup.BUILDING_BLOCKS);
+                                                     new Block(FabricBlockSettings.of(Material.ICE).hardness(0.15f).resistance(0.6f).nonOpaque(), ItemGroup.BUILDING_BLOCKS);
 
 public static final Block ORIENTAL_WINDOW_PANEL = registerBlock("oriental_window_panel",
                                                                 new ModPaneBlock(FabricBlockSettings.of(Material.GLASS).hardness(0.3f).resistance(0.3f).nonOpaque().sounds(BlockSoundGroup.GLASS)), ItemGroup.BUILDING_BLOCKS);
